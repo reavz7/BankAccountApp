397010c00e3b8d34da852054f3632752b86a8f66
reavz7 <liuszkemi75@gmail.com>
2025-03-06 12:03:17 +0100

    Interface

--- BankAccountApp/Form1.Designer.cs
+++ BankAccountApp/Form1.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            OwnerTxt = new TextBox();
+            AmountNum = new NumericUpDown();
+            aq = new DataGridView();
+            DepositBtn = new Button();
+            WithdrawBtn = new Button();
+            CreateAccountBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)AmountNum).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)aq).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label1.Location = new Point(38, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Owner:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label2.Location = new Point(40, 217);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Amonut:";
+            // 
+            // OwnerTxt
+            // 
+            OwnerTxt.Location = new Point(140, 24);
+            OwnerTxt.Name = "OwnerTxt";
+            OwnerTxt.Size = new Size(182, 23);
+            OwnerTxt.TabIndex = 2;
+            // 
+            // AmountNum
+            // 
+            AmountNum.Location = new Point(140, 222);
+            AmountNum.Name = "AmountNum";
+            AmountNum.Size = new Size(182, 23);
+            AmountNum.TabIndex = 3;
+            // 
+            // aq
+            // 
+            aq.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            aq.Location = new Point(392, 24);
+            aq.Name = "aq";
+            aq.Size = new Size(431, 192);
+            aq.TabIndex = 4;
+            // 
+            // DepositBtn
+            // 
+            DepositBtn.Location = new Point(392, 222);
+            DepositBtn.Name = "DepositBtn";
+            DepositBtn.Size = new Size(229, 50);
+            DepositBtn.TabIndex = 5;
+            DepositBtn.Text = "Deposit";
+            DepositBtn.UseVisualStyleBackColor = true;
+            // 
+            // WithdrawBtn
+            // 
+            WithdrawBtn.Location = new Point(627, 222);
+            WithdrawBtn.Name = "WithdrawBtn";
+            WithdrawBtn.Size = new Size(196, 50);
+            WithdrawBtn.TabIndex = 6;
+            WithdrawBtn.Text = "Withdraw";
+            WithdrawBtn.UseVisualStyleBackColor = true;
+            // 
+            // CreateAccountBtn
+            // 
+            CreateAccountBtn.Location = new Point(140, 79);
+            CreateAccountBtn.Name = "CreateAccountBtn";
+            CreateAccountBtn.Size = new Size(182, 50);
+            CreateAccountBtn.TabIndex = 7;
+            CreateAccountBtn.Text = "Create account";
+            CreateAccountBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(845, 302);
+            Controls.Add(CreateAccountBtn);
+            Controls.Add(WithdrawBtn);
+            Controls.Add(DepositBtn);
+            Controls.Add(aq);
+            Controls.Add(AmountNum);
+            Controls.Add(OwnerTxt);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "a";
+            ((System.ComponentModel.ISupportInitialize)AmountNum).EndInit();
+            ((System.ComponentModel.ISupportInitialize)aq).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox OwnerTxt;
+        private NumericUpDown AmountNum;
+        private DataGridView aq;
+        private Button DepositBtn;
+        private Button WithdrawBtn;
+        private Button CreateAccountBtn;
     }
 }
