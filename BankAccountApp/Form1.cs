17f6803bf0c7a1cf773f2a13fc104dd057c8f257
reavz7 <liuszkemi75@gmail.com>
2025-03-06 13:54:54 +0100

    RefreshGrid method

--- BankAccountApp/Form1.cs
+++ BankAccountApp/Form1.cs
@@ -4,25 +4,43 @@ namespace BankAccountApp
 {
     public partial class Form1 : Form
     {
+        List<BankAccount> BankAccounts = new List<BankAccount>();
+
         public Form1()
         {
             InitializeComponent();
             BankAccount bankAccount = new BankAccount("Kamil Stêpieñ");
             BankAccount bankAccount2 = new BankAccount("Adam Mickiewcz");
-            BankAccount bankAccount3 = new BankAccount("Cristiano Ronaldo");
-
-            List<BankAccount> bankAccounts = new List<BankAccount>();
-            bankAccounts.Add(bankAccount);
-            bankAccounts.Add(bankAccount2);
-            bankAccounts.Add(bankAccount3);
-
-            BankAccountsGrid.DataSource = bankAccounts;     
+            BankAccounts.Add(bankAccount);
+            BankAccounts.Add(bankAccount2); 
+            RefreshGrid();
         }
-       
-            
+
+
         private void label1_Click(object sender, EventArgs e)
         {
+                
+        }
+            
+        private void CreateAccountBtn_Click(object sender, EventArgs e)
+        {
+            BankAccount bankAccount = new BankAccount(OwnerTxt.Text);
+            if (OwnerTxt.Text == "" || OwnerTxt.Text == null)
+            {
+                MessageBox.Show("Nie wprowadzono imienia i nazwiska");
+            }
+            else { 
+            BankAccounts.Add(bankAccount);  
+            RefreshGrid();  
+            OwnerTxt.Clear();
+            }
+        }
+        private void RefreshGrid() {
+
+            BankAccountsGrid.DataSource = null;
+            BankAccountsGrid.DataSource = BankAccounts;
 
         }
+        
     }
 }
